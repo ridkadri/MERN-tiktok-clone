128425a5a996e8ed164ced8893298609225bb044
Ridwan <kadriridwan0@gmail.com>
2020-11-14 11:08:49 +0000

    added footer roll ticker functionality, spinning record animation to footer and also added sidebar icons

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -4,6 +4,8 @@ import './Video.css';
 import VideoFooter from '../VideoFooter/VideoFooter';
 import VideoSidebar from '../VideoSidebar/VideoSidebar';
 
+
+
 const Video = () => {
     const [playing, setPlaying] = useState(false);
     const videoRef = useRef(null);
@@ -29,8 +31,9 @@ const Video = () => {
                 className='video_player'
                 loop
                 ref={videoRef}
-                src='https://v77.tiktokcdn.com/b295c01060f75123ffa2af1dfc7fcd5a/5faf1531/video/tos/alisg/tos-alisg-pve-0037c001/a2ffe1dc0c634b5e9293ce484fd7c38e/?a=1233&br=5444&bt=2722&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20201113172154010189073152260216F3&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=amp5cWRscDM0eDMzZzczM0ApODxlZjszOmQzN2k8Nzs5ZWdec2U1Ly5wcjVfLS1hMTRzc2IuYl4tL2EvYl4zNmIyYGE6Yw%3D%3D&vl=&vr='>
+                src='https://v77.tiktokcdn.com/40c98496a7bf378b216bb4e41a0384d5/5fb0069b/video/tos/useast2a/tos-useast2a-ve-0068c002/1445c82d11ea45b79d4cac909a98e6d6/?a=1233&br=1638&bt=819&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020111410320501019018603629089A6C&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2g5a3A8dTNqeDMzNjczM0ApOzY6ZmhkPDs7NztlNDs3N2dnLTZfZC1eaGtfLS02MTZzczEtMTBfMjFjMTUwMDZjYzA6Yw%3D%3D&vl=&vr='>
             </video>
+            
             <VideoFooter/>
             <VideoSidebar/>
         </div>
--- src/components/VideoSidebar/VideoSidebar.js
+++ src/components/VideoSidebar/VideoSidebar.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './VideoSidebar.css';
 
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MessageIcon from '@material-ui/icons/Message';
+import ShareIcon from '@material-ui/icons/Share';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+
 const VideoSidebar = () => {
+    const [liked, setLiked] = useState(false);
+
     return (
         <div className='videoSidebar'>
-            
+            <div className='videoSidebar_button'>
+                {liked? (
+                    <FavoriteIcon fontSize='large'/>
+                ) : (
+                    <FavoriteBorderIcon 
+                        fontSize='large' 
+                        onClick={(e)=>setLiked(true)}
+                    />
+                )}
+                <p>100</p>
+            </div>
+            <div className='videoSidebar_button'>
+                <MessageIcon fontSize='large'/>
+                <p>250</p>
+            </div>
+            <div className='videoSidebar_button'>
+                <ShareIcon fontSize='large'/>
+                <p>12</p>
+            </div>
         </div>
     )
 }
